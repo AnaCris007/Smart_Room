a5612640c8d96ca81e95558225bcf2573d34d29a
Ana Cristina Alves Jardim <ana.jardim@sou.inteli.edu.br>
2025-06-06 01:08:44 -0300

    feat: controllers e routes das views

---
+++ controllers/ViewsController.js
@@ -0,0 +1,11 @@
+exports.renderCadastro = (req, res) => {
+    res.render('Cadastro');
+};
+
+exports.renderLogin = (req, res) => {
+    res.render('Login');
+};
+
+exports.renderReservas = (req, res) => {
+    res.render('Reservas');
+};
